4b9b4f00b3868816889e3273230e33d76b9f2e57
Amandeep Singh <31513195+amanjosan2008@users.noreply.github.com>
2018-12-13 20:25:50 +0530

    Update tech_analyzer.py

--- tech_analyzer.py
+++ tech_analyzer.py
@@ -4,14 +4,15 @@
 import cgi, cgitb
 
 print ("Content-type:text/html\r\n\r\n")
-print ("<html>")
-print ("<head>")
-print ("<title>Tech Support Debug Logs Analyzer</title>")
-print ("</head>")
-print ("<body>")
-print('<form action = "/cgi-bin/results.py" method = "get">')
-print('Logs Location: <input type = "text" name = "path">  <br />')
-print('<input type = "submit" value = "Submit" />')
-print("</form>")
-print ("</body>")
-print ("</html>")
+print ("""
+<html>
+<head>
+<title>Tech Support Debug Logs Analyzer</title>
+</head>
+<body style='background-color:d9d9d9;'>
+<form action = "/cgi-bin/results.py" method = "get">
+Logs Location: <input type = "text" name = "path">  <br />
+<input type = "submit" value = "Submit" />
+</form>
+</body>
+</html>""")
